8d5f04d10448cb1202991ea7ecce37f11179593d
James Starkman <-1@case.edu>
2017-06-16 17:06:03 -0400

    LIDAR alarm: untested (b/c no STDR) but seems to work.

--- Part_2/lidar_alarm/src/lidar_alarm.cpp
+++ Part_2/lidar_alarm/src/lidar_alarm.cpp
@@ -1,73 +1,67 @@
-// wsn example program to illustrate LIDAR processing.  1/23/15
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/float32.hpp"
 
-#include <ros/ros.h> //Must include this for all ROS cpp projects
-#include <sensor_msgs/LaserScan.h>
-#include <std_msgs/Float32.h> //Including the Float32 class from std_msgs
-#include <std_msgs/Bool.h> // boolean message 
+const double MIN_SAFE_DISTANCE = 1.0;
 
-
-const double MIN_SAFE_DISTANCE = 1.0; // set alarm if anything is within 0.5m of the front of robot
-
-// these values to be set within the laser callback
-float ping_dist_in_front_=3.0; // global var to hold length of a SINGLE LIDAR ping--in front
-int ping_index_= -1; // NOT real; callback will have to find this
-double angle_min_=0.0;
-double angle_max_=0.0;
-double angle_increment_=0.0;
+float ping_dist_in_front_ = 3.0;
+int ping_index_ = -1;
+double angle_min_ = 0.0;
+double angle_max_ = 0.0;
+double angle_increment_ = 0.0;
 double range_min_ = 0.0;
 double range_max_ = 0.0;
-bool laser_alarm_=false;
+bool laser_alarm_ = false;
 
-ros::Publisher lidar_alarm_publisher_;
-ros::Publisher lidar_dist_publisher_;
-// really, do NOT want to depend on a single ping.  Should consider a subset of pings
-// to improve reliability and avoid false alarms or failure to see an obstacle
+rclcpp::publisher::Publisher<std_msgs::msg::Bool>::SharedPtr lidar_alarm_publisher_;
+rclcpp::publisher::Publisher<std_msgs::msg::Float32>::SharedPtr lidar_dist_publisher_;
 
-void laserCallback(const sensor_msgs::LaserScan& laser_scan) {
-    if (ping_index_<0)  {
-        //for first message received, set up the desired index of LIDAR range to eval
-        angle_min_ = laser_scan.angle_min;
-        angle_max_ = laser_scan.angle_max;
-        angle_increment_ = laser_scan.angle_increment;
-        range_min_ = laser_scan.range_min;
-        range_max_ = laser_scan.range_max;
-        // what is the index of the ping that is straight ahead?
-        // BETTER would be to use transforms, which would reference how the LIDAR is mounted;
-        // but this will do for simple illustration
-        ping_index_ = (int) ((0.0 -angle_min_)/angle_increment_);
-        ROS_INFO("LIDAR setup: ping_index = %d",ping_index_);
-        
-    }
-    
-   ping_dist_in_front_ = laser_scan.ranges[ping_index_];
-   ROS_INFO("ping dist in front = %f",ping_dist_in_front_);
-   if (ping_dist_in_front_<MIN_SAFE_DISTANCE) {
-       ROS_WARN("DANGER, WILL ROBINSON!!");
-       laser_alarm_=true;
-   }
-   else {
-       laser_alarm_=false;
-   }
-   std_msgs::Bool lidar_alarm_msg;
-   lidar_alarm_msg.data = laser_alarm_;
-   lidar_alarm_publisher_.publish(lidar_alarm_msg);
-   std_msgs::Float32 lidar_dist_msg;
-   lidar_dist_msg.data = ping_dist_in_front_;
-   lidar_dist_publisher_.publish(lidar_dist_msg);   
+// auto my_publisher = node->create_publisher<std_msgs::msg::Float64>(
+//       "force_cmd", rmw_qos_profile_default);
+
+void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr laser_scan)
+{
+  if (ping_index_ < 0)
+  {
+    angle_min_ = laser_scan->angle_min;
+    angle_max_ = laser_scan->angle_max;
+    angle_increment_ = laser_scan->angle_increment;
+    range_min_ = laser_scan->range_min;
+    range_max_ = laser_scan->range_max;
+
+    ping_index_ = (int)((0.0 - angle_min_) / angle_increment_);
+    std::cout << "LIDAR setup: ping_index = " << ping_index_ << std::endl;
+  }
+
+  ping_dist_in_front_ = laser_scan->ranges[ping_index_];
+  std::cout << "ping dist in front = " << ping_dist_in_front_ << std::endl;
+  if (ping_dist_in_front_ < MIN_SAFE_DISTANCE)
+  {
+    std::cout << "DANGER, WILL ROBINSON!!" << std::endl;
+    laser_alarm_ = true;
+  }
+  else
+  {
+    laser_alarm_ = false;
+  }
+  std_msgs::msg::Bool lidar_alarm_msg;
+  lidar_alarm_msg.data = laser_alarm_;
+  lidar_alarm_publisher_->publish(lidar_alarm_msg);
+
+  std_msgs::msg::Float32 lidar_dist_msg;
+  lidar_dist_msg.data = ping_dist_in_front_;
+  lidar_dist_publisher_->publish(lidar_dist_msg);
 }
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "lidar_alarm"); //name this node
-    ros::NodeHandle nh; 
-    //create a Subscriber object and have it subscribe to the lidar topic
-    ros::Publisher pub = nh.advertise<std_msgs::Bool>("lidar_alarm", 1);
-    lidar_alarm_publisher_ = pub; // let's make this global, so callback can use it
-    ros::Publisher pub2 = nh.advertise<std_msgs::Float32>("lidar_dist", 1);  
-    lidar_dist_publisher_ = pub2;
-    ros::Subscriber lidar_subscriber = nh.subscribe("robot0/laser_0", 1, laserCallback);
-    ros::spin(); //this is essentially a "while(1)" statement, except it
-    // forces refreshing wakeups upon new data arrival
-    // main program essentially hangs here, but it must stay alive to keep the callback function alive
-    return 0; // should never get here, unless roscore dies
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::node::Node::make_shared("lidar_alarm");
+  lidar_alarm_publisher_ = node->create_publisher<std_msgs::msg::Bool>("lidar_alarm", rmw_qos_profile_default);
+  lidar_dist_publisher_ = node->create_publisher<std_msgs::msg::Float32>("lidar_dist", rmw_qos_profile_default);
+  auto lidar_subscriber =
+      node->create_subscription<sensor_msgs::msg::LaserScan>("robot0/laser_0", laserCallback, rmw_qos_profile_default);
+  rclcpp::spin(node);
+  return 0;
 }
-
