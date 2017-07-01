de49c9d594c2edfd9820a5e25148452833fc548b
James Starkman <-1@case.edu>
2017-07-01 18:47:59 -0400

    Fix formatting for Part_1.

--- Part_1/example_ros_service/src/path_service.cpp
+++ Part_1/example_ros_service/src/path_service.cpp
@@ -1,8 +1,8 @@
-#include "nav_msgs/msg/path.hpp"
 #include <iostream>
 #include <string>
 #include "example_ros_service/srv/path_srv.hpp"
 #include "geometry_msgs/msg/pose.hpp"
+#include "nav_msgs/msg/path.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 /* ugly hack until rosconsole works */
