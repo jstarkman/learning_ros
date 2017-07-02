82299dea0a7033ccc4420eef6d959f54f778005b
James Starkman <-1@case.edu>
2017-07-01 23:30:04 -0400

    Part_5: baxter: ported baxter_head_pan and enabled bridging to ROS1.

---
+++ Part_5/baxter/README.md
@@ -0,0 +1,17 @@
+# Baxter
+
+Baxter itself runs ROS1.  To talk to it via the bridge, edit
+`/path/to/your/ros2_ws/src/ros2/ros1_bridge/CMakeLists.txt`
+such that it contains the new line below.
+
+```cmake
+include_directories(include ${generated_path})
+# new line below (change the path accordingly)
+include_directories(include /path/to/your/ros_ws/devel/include/)
+```
+
+# Unported
+
+The following packages are unported:
+
+baxter_jnt_traj_ctlr_client
--- Part_5/baxter/baxter_head_pan/src/baxter_head_pan.cpp
+++ Part_5/baxter/baxter_head_pan/src/baxter_head_pan.cpp
@@ -1,36 +1,40 @@
-#include <ros/ros.h>
-#include <baxter_core_msgs/HeadPanCommand.h>
-using namespace std;
+#include <iostream>
+#include <memory>
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "baxter_head_pan"); 
-    ros::NodeHandle n; 
-    //create a publisher to send commands to Baxter's head pan
-    ros::Publisher head_pan_pub = n.advertise<baxter_core_msgs::HeadPanCommand>("/robot/head/command_head_pan", 1);
+#include "rclcpp/rclcpp.hpp"
 
-    baxter_core_msgs::HeadPanCommand headPanCommand; //message type for head-pan control
-    headPanCommand.target = 0.0;
-    headPanCommand.enable_pan_request=1;
-    //prompt user for amplitude and frequency of oscillating head pan
-    double amp,freq;
-    cout<<"enter pan amplitude (rad): ";
-    cin>>amp;
-    cout<<"enter pan freq (Hz): ";
-    cin>>freq;
-    double dt = 0.01;
-    ros::Rate timer(1/dt);
-    double phase=0.0;
-    double theta;
-    
-    // oscillate head pan indefinitely
-    while (ros::ok()) 
-    {
-        phase+= M_PI*2.0*freq*dt;
-        if (phase>2.0*M_PI) phase-= 2.0*M_PI;
-        theta = amp*sin(phase);
-        headPanCommand.target = theta;
-        head_pan_pub.publish(headPanCommand);
-        timer.sleep();
-    }
+#include "baxter_core_msgs/msg/head_pan_command.hpp"
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::node::Node::make_shared("baxter_head_pan");
+  auto head_pan_pub = node->create_publisher<baxter_core_msgs::msg::HeadPanCommand>("/robot/head/command_head_pan",
+                                                                                    rmw_qos_profile_default);
+
+  auto headPanCommand = std::make_shared<baxter_core_msgs::msg::HeadPanCommand>();
+  headPanCommand->target = 0.0;
+  headPanCommand->enable_pan_request = 1;
+
+  double amp, freq;
+  std::cout << "enter pan amplitude (rad): ";
+  std::cin >> amp;
+  std::cout << "enter pan freq (Hz): ";
+  std::cin >> freq;
+  double dt = 0.01;
+  rclcpp::WallRate timer(1 / dt);
+  double phase = 0.0;
+  double theta;
+
+  while (rclcpp::ok())
+  {
+    phase += M_PI * 2.0 * freq * dt;
+    if (phase > 2.0 * M_PI)
+      phase -= 2.0 * M_PI;
+    theta = amp * sin(phase);
+    headPanCommand->target = theta;
+    head_pan_pub->publish(headPanCommand);
+    timer.sleep();
+    rclcpp::spin_some(node);
+  }
 }
-
