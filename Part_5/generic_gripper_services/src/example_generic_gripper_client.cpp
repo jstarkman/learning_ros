3b236b26caefef8abbe0a4b225e6f1fdd4eb9e3d
James Starkman <-1@case.edu>
2017-07-03 03:13:21 -0400

    Part_5: generic_gripper_services: ported, but may need to change package.xml in the future.

--- Part_5/generic_gripper_services/src/example_generic_gripper_client.cpp
+++ Part_5/generic_gripper_services/src/example_generic_gripper_client.cpp
@@ -1,42 +1,73 @@
-//example_generic_gripper_client.cpp
-//wsn Nov 2016
-//example of how to communicate with generic gripper interface,
-//requires that a gripper service is running
-#include<ros/ros.h>
+#include "generic_gripper_services/srv/generic_gripper_interface.hpp"
+#include "rclcpp/rclcpp.hpp"
 
-#include<generic_gripper_services/genericGripperInterface.h>
+#define ROS_INFO printf
+#define ROS_WARN printf
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "generic_gripper_interface_client");
-    ros::NodeHandle n;
-    ros::ServiceClient client = 
-      n.serviceClient<generic_gripper_services::genericGripperInterface>("generic_gripper_svc");
-    generic_gripper_services::genericGripperInterface srv;
-    srv.request.cmd_code = generic_gripper_services::genericGripperInterfaceRequest::TEST_PING;
-    ROS_INFO("sending a test ping");
-    bool success=false;
-    while (!success) {
-         client.call(srv); 
-         success = srv.response.success;
-         ROS_INFO("retrying...is the gripper service running?");
-         ros::Duration(0.5).sleep();
-         ros::spinOnce();
-    }
-    ROS_INFO("got test ping response");
-    
-    ROS_INFO("try sending release command: ");
-    srv.request.cmd_code = generic_gripper_services::genericGripperInterfaceRequest::RELEASE;
-    client.call(srv); 
-    success = srv.response.success;
-    if (success) { ROS_INFO("responded w/ success"); }
-    else {ROS_WARN("responded with failure"); }
+generic_gripper_services::srv::GenericGripperInterface_Response::SharedPtr
+send_blocking_request(rclcpp::node::Node::SharedPtr node,
+                      rclcpp::client::Client<generic_gripper_services::srv::GenericGripperInterface>::SharedPtr client,
+                      generic_gripper_services::srv::GenericGripperInterface_Request::SharedPtr request)
+{
+  auto response = client->async_send_request(request);
+  if (rclcpp::spin_until_future_complete(node, response) == rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
+    return response.get();
+  }
+  else
+  {
+    return NULL;
+  }
+}
 
-    ROS_INFO("try sending GRASP command: ");
-    srv.request.cmd_code = generic_gripper_services::genericGripperInterfaceRequest::GRASP;
-    client.call(srv); 
-    success = srv.response.success;
-    if (success) { ROS_INFO("responded w/ success"); }
-    else {ROS_WARN("responded with failure"); }
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
 
-    return 0;
-}    
+  auto node = rclcpp::node::Node::make_shared("generic_gripper_interface_client");
+  auto client = node->create_client<generic_gripper_services::srv::GenericGripperInterface>("generic_gripper_svc");
+
+  auto request = std::make_shared<generic_gripper_services::srv::GenericGripperInterface::Request>();
+  request->cmd_code = generic_gripper_services::srv::GenericGripperInterface::Request::TEST_PING;
+
+  ROS_INFO("sending a test ping");
+  bool success = false;
+  generic_gripper_services::srv::GenericGripperInterface_Response::SharedPtr response;
+  while (!success)
+  {
+    response = send_blocking_request(node, client, request);
+    success = response->success;
+    ROS_INFO("retrying...is the gripper service running?");
+    rclcpp::WallRate(2).sleep();
+    rclcpp::spin_some(node);
+  }
+  ROS_INFO("got test ping response");
+
+  ROS_INFO("try sending release command: ");
+  request->cmd_code = generic_gripper_services::srv::GenericGripperInterface::Request::RELEASE;
+  response = send_blocking_request(node, client, request);
+  success = response->success;
+  if (success)
+  {
+    ROS_INFO("responded w/ success");
+  }
+  else
+  {
+    ROS_WARN("responded with failure");
+  }
+
+  ROS_INFO("try sending GRASP command: ");
+  request->cmd_code = generic_gripper_services::srv::GenericGripperInterface::Request::GRASP;
+  response = send_blocking_request(node, client, request);
+  success = response->success;
+  if (success)
+  {
+    ROS_INFO("responded w/ success");
+  }
+  else
+  {
+    ROS_WARN("responded with failure");
+  }
+
+  return 0;
+}
--- Part_5/generic_gripper_services/src/rethink_rt_gripper_service.cpp
+++ Part_5/generic_gripper_services/src/rethink_rt_gripper_service.cpp
@@ -1,82 +1,80 @@
-//rethink_rt_gripper_service.cpp
-//wsn Nov 2016
-//provides a service to process gripper options, intended to use a generic gripper interface,
-//but specifically controls the Rethink right gripper
-#include<ros/ros.h>
-#include<simple_baxter_gripper_interface/simple_baxter_gripper_interface.h>
-#include<generic_gripper_services/genericGripperInterface.h>
-BaxterGripper *baxterGripper_ptr;
-const double MAX_WAIT_TIME = 3.0; //put a limit on how long will wait for gripper open/close
-const double DT=0.01;
+#include "generic_gripper_services/srv/generic_gripper_interface.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "simple_baxter_gripper_interface/simple_baxter_gripper_interface.hpp"
 
+#define ROS_INFO printf
+#define ROS_WARN printf
 
-bool callback(generic_gripper_services::genericGripperInterfaceRequest& request,
-        generic_gripper_services::genericGripperInterfaceResponse& response) {
-        int command_code = request.cmd_code;
-        //int ntries = 0;
-        double wait_time=0;
+const double MAX_WAIT_TIME = 3.0;
+const double DT = 0.01;
 
-        switch(command_code) {
-           case generic_gripper_services::genericGripperInterfaceRequest::TEST_PING:
-             ROS_INFO("gripper service received a test ping");
-             response.success = true; 
-             return true;
-             break;
-           case generic_gripper_services::genericGripperInterfaceRequest::RELEASE:  
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::node::Node::make_shared("generic_gripper_interface_svc");
+  BaxterGripper baxterGripper(node);
+  auto service = node->create_service<generic_gripper_services::srv::GenericGripperInterface>(
+      "generic_gripper_svc",
+      [&, node](const std::shared_ptr<rmw_request_id_t> request_header,
+                const generic_gripper_services::srv::GenericGripperInterface::Request::SharedPtr request,
+                generic_gripper_services::srv::GenericGripperInterface::Response::SharedPtr response) {
+        int command_code = request->cmd_code;
+
+        double wait_time = 0;
+
+        switch (command_code)
+        {
+          case generic_gripper_services::srv::GenericGripperInterface::Request::TEST_PING:
+            ROS_INFO("gripper service received a test ping");
+            response->success = true;
+            break;
+          case generic_gripper_services::srv::GenericGripperInterface::Request::RELEASE:
             ROS_INFO("opening right gripper");
-            baxterGripper_ptr->right_gripper_open();
-            ros::spinOnce();
-    
-            ROS_INFO("right gripper pos = %f; waiting for pos>95", baxterGripper_ptr->get_right_gripper_pos());
-            while (baxterGripper_ptr->get_right_gripper_pos() < 95.0) {
-                wait_time+=DT;
-                baxterGripper_ptr->right_gripper_open();
-                ros::spinOnce();
-                //ROS_INFO("gripper pos = %f", baxterGripper_ptr->get_right_gripper_pos());
-                ros::Duration(DT).sleep();
-                if (wait_time>MAX_WAIT_TIME) {
-                    ROS_WARN("giving up waiting on gripper opening");
-                    response.success = false; 
-                    return true;   
-                }
+            baxterGripper.right_gripper_open();
+            // rclcpp::spin_some(node);
+
+            ROS_INFO("right gripper pos = %f; waiting for pos>95", baxterGripper.get_right_gripper_pos());
+            while (baxterGripper.get_right_gripper_pos() < 95.0)
+            {
+              wait_time += DT;
+              baxterGripper.right_gripper_open();
+              // rclcpp::spin_some(node);
+
+              rclcpp::WallRate(1 / DT).sleep();
+              if (wait_time > MAX_WAIT_TIME)
+              {
+                ROS_WARN("giving up waiting on gripper opening");
+                response->success = false;
+                return;
               }
-              //if here, then detected gripper is open:
-              ROS_INFO("gripper is open");
-              ROS_INFO("gripper pos = %f", baxterGripper_ptr->get_right_gripper_pos());
-              response.success = true; 
-              return true;               
-           case generic_gripper_services::genericGripperInterfaceRequest::GRASP_W_PARAMS:
-             ROS_WARN("grasp_w_params command not implemented");
-             response.success = false; 
-             return true;             
-           
-           case generic_gripper_services::genericGripperInterfaceRequest::GRASP: 
-               ROS_INFO("closing right gripper");
-               baxterGripper_ptr->right_gripper_close();
-               ros::Duration(1.0).sleep();
-                response.success = true; 
-             return true;             
-           case generic_gripper_services::genericGripperInterfaceRequest::TEST_GRASP: 
-             ROS_WARN("grasp test not implemented");
-             //should look at provided params and decide if gripper pos is between these bounds
-             // params are object-dependent; should be obtained from manipulation_properties service
-             response.success = false; 
-             return true;             
-           default:
-             ROS_WARN("gripper command not recognized");
-             response.success = false; 
-             return true;
-           }            
+            }
+
+            ROS_INFO("gripper is open");
+            ROS_INFO("gripper pos = %f", baxterGripper.get_right_gripper_pos());
+            response->success = true;
+            break;
+          case generic_gripper_services::srv::GenericGripperInterface::Request::GRASP_W_PARAMS:
+            ROS_WARN("grasp_w_params command not implemented");
+            response->success = false;
+            break;
+          case generic_gripper_services::srv::GenericGripperInterface::Request::GRASP:
+            ROS_INFO("closing right gripper");
+            baxterGripper.right_gripper_close();
+            rclcpp::WallRate(1.0).sleep();
+            response->success = true;
+            break;
+          case generic_gripper_services::srv::GenericGripperInterface::Request::TEST_GRASP:
+            ROS_WARN("grasp test not implemented");
+            response->success = false;
+            break;
+          default:
+            ROS_WARN("gripper command not recognized");
+            response->success = false;
+            break;
+        }
+      });
+  ROS_INFO("generic gripper service ready; this version customized for Rethink right gripper");
+  rclcpp::spin(node);
+
+  return 0;
 }
-
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "generic_gripper_interface_svc");
-    ros::NodeHandle n;
-    BaxterGripper baxterGripper(&n);
-    baxterGripper_ptr = &baxterGripper; // make this class accessible to callback   
-    ros::ServiceServer service = n.advertiseService("generic_gripper_svc", callback);
-    ROS_INFO("generic gripper service ready; this version customized for Rethink right gripper");
-    ros::spin();
-
-    return 0;
-}    
