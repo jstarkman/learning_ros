c3356b41ca538b10723ebb7cd8d6c741b7fd1b4e
James Starkman <jstarkman@users.noreply.github.com>
2017-07-20 03:04:02 -0400

    Pedant: learning about C++11 lambdas

--- Part_1/minimal_nodes/src/minimal_subscriber_lambda.cpp
+++ Part_1/minimal_nodes/src/minimal_subscriber_lambda.cpp
@@ -12,6 +12,15 @@ int main(int argc, char **argv)
 
   auto sub = node->create_subscription<std_msgs::msg::Float64>(
       "topic1",
+      /* Example of a closure, introduced in C++11.  Outer variables
+       * to be enclosed within the lambda's scope go in the [] (here
+       * there are none, hence the empty brackets).  Function
+       * arguments go between the () and code between the {} as usual.
+       * For more examples, see other packages, e.g.,
+       * Part_4/mobot_pub_des_state/src/pub_des_state.cpp
+       * (those are for services, but services take callbacks too,
+       * and thus also take lambda expressions).
+       */
       [](const std_msgs::msg::Float64::SharedPtr msg) { std::cout << "received value is: " << msg->data << std::endl; },
       rmw_qos_profile_default);
 
