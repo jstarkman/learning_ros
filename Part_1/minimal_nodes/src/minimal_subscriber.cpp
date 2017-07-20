a0e29783d2810f42e3fd30131cd4841f75b14877
James Starkman <jstarkman@users.noreply.github.com>
2017-07-20 02:45:13 -0400

    Pedant: explained callbacks and mentioned lambdas

--- Part_1/minimal_nodes/src/minimal_subscriber.cpp
+++ Part_1/minimal_nodes/src/minimal_subscriber.cpp
@@ -5,6 +5,12 @@
 
 #include "std_msgs/msg/float64.hpp"
 
+/**
+ * Callback run when a new Float64 comes in.  This is the function
+ * referred to by the `create_subscription` line.  Simple callbacks
+ * like this one should be implemented as lambda expressions (see
+ * minimal_subscriber_lambda.cpp for details).
+ */
 void cbTopic1(const std_msgs::msg::Float64::SharedPtr msg)
 {
   printf("received value is: %f\n", msg->data);
